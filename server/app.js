8ae024397e48cea375afcd66a91163e668251750
piku5445 <pikupranjal888@gmail.com>
2026-03-09 14:14:44 +0530

    fix: configure CORS to allow requests from localhost with credentials

--- server/app.js
+++ server/app.js
@@ -12,7 +12,10 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-app.use(cors());
+app.use(cors({
+ origin: "http://localhost:5173",
+ credentials: true
+}));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
